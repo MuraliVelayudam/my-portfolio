2cfe3cff2e9365726f511086ac00281048a21dfa
MuraliVelayudam <murali.velayudam@gmail.com>
2024-06-02 09:35:46 +0530

    contact update

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,42 +1,14 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { useActiveSectionContext } from '@/context-section/section-context-provider'
-import toast from 'react-hot-toast'
 import ContactForm from './contactForm'
 import SectionHeading from './sectionHeading'
 
 export default function Contact() {
     const { lastTimeOfClick, setActiveSection } = useActiveSectionContext()
     const { ref, inView } = useInView({ threshold: 0.8 })
-
-    const emailRef = useRef(null)
-
-    const onHandleCopyToClipboard = () => {
-        if (emailRef.current) {
-            const email = emailRef.current.innerText
-
-            if (navigator.clipboard) {
-                navigator.clipboard
-                    .writeText(email)
-                    .then(() => {
-                        toast.success('Email copied to clipboard!')
-                    })
-                    .catch((error) => {
-                        console.error('Error copying to clipboard:', error)
-                    })
-            } else {
-                const textarea = document.createElement('textarea')
-                textarea.value = email
-                document.body.appendChild(textarea)
-                textarea.select()
-                document.execCommand('copy')
-                document.body.removeChild(textarea)
-                toast.success('Email copied to clipboard!')
-            }
-        }
-    }
 
     useEffect(() => {
         if (inView && Date.now() - lastTimeOfClick > 1000) {
@@ -56,13 +28,12 @@ export default function Contact() {
             <SectionHeading>Contact Me</SectionHeading>
             <p className="text-gray-500 text-sm text-center dark:text-white/80">
                 Please contact me directly at{' '}
-                <span
-                    ref={emailRef}
-                    onClick={onHandleCopyToClipboard}
+                <a
+                    href="mailto:murali.krishna01294@gmail.com"
                     className="font-semibold text-gray-950 italic animate-pulse cursor-pointer dark:text-white"
                 >
                     murali.krishna01294@gmail.com
-                </span>{' '}
+                </a>{' '}
                 or through this form.
             </p>
 
